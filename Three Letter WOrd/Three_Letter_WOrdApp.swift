998e6d8470201c2cee8162f3260df1d925911ba9
Alex Anderson <andersonaj116@gmail.com>
2022-10-26 09:28:09 -0500

    Initial Commit

---
+++ Three Letter WOrd/Three_Letter_WOrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_WOrdApp.swift
+//  Three Letter WOrd
+//
+//  Created by Alex Anderson on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_WOrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
